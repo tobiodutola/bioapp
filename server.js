1f2a5190eaa43c92ac073b4ca4f65dde068e7d50
oluwatobi <oluwatobyodutola@gmail.com>
2022-10-27 23:54:56 +0100

    done1

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cors =require("cors");
 const PORT =process.env.PORT || 3030
 
+app.use(cors({credentials:true, origin:true}));
 
 app.get("/", (req,res)=>{
     res.json({"slackUsername":"pherahrhi","backend":true,"age":27,"biography":"willing to learn and supports manchester united"})
