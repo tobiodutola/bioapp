40f334301e7bc3a5e63673b3b67686b2ca9770fe
oluwatobi <oluwatobyodutola@gmail.com>
2022-10-27 23:14:26 +0100

    dd

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const PORT =process.env.PORT || 3030
+
+
+app.get("/", (req,res)=>{
+    res.json({"slackUsername":"pherahrhi","backend":true,"age":27,"biography":"willing to learn and supports manchester united"})
+})
+
+app.listen(PORT);
